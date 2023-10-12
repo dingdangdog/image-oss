eaa9d80a44475916b6b39bee9fad292aba56ebfc
ddd <dddogx@qq.com>
2023-10-12 14:42:41 +0800

    成功/失败提示浮窗造轮子

---
+++ alert-cus.js
@@ -0,0 +1,24 @@
+function alertSuccess(msg, duration) {
+    alertCust(msg, "success-msg", duration);
+}
+
+
+function alertError(msg, duration) {
+    alertCust(msg, "error-msg", duration);
+}
+
+function alertCust(msg, className, duration) {
+    console.log(msg, className);
+    const msgContainer = document.getElementById("msg-container");
+
+    let msgDiv = document.createElement("div");
+    msgDiv.innerText = msg;
+    msgDiv.className = className;
+    msgContainer.appendChild(msgDiv);
+    msgContainer.style.display = "block";
+
+    window.setTimeout(function () {
+        msgContainer.removeChild(msgDiv);
+        msgContainer.style.display = "none";
+    }, duration)
+}
